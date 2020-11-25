ab1f6897b5533e02a5d9cf25d349d07b70952645
Andrew Davis <atd285@gmail.com>
2020-11-25 17:46:01 -0500

    finish database setup with monk

--- src/api/thanks.js
+++ src/api/thanks.js
@@ -17,8 +17,37 @@ const schema = Joi.object({
     ),
 });
 
-router.get('/', (req, res, next) => {
-  res.send('hello thanks!');
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const found = await thanks.findOne({
+      slug: req.params.slug,
+    });
+    if (found) {
+      return res.json(found);
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const values = await schema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    if (!values.slug) {
+      values.slug = nanoid();
+    }
+    values.slug = values.slug.toLowerCase();
+    const created = await thanks.insert(values);
+    return res.json(created);
+  } catch (error) {
+    if (error.message.startsWith('E11000 duplicate key error collection')) {
+      error.message = 'Error! Slug in use.';
+    }
+    return next(error);
+  }
 });
 
 module.exports = router;
